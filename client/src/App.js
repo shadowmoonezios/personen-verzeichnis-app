efb964bbac36c456b8ac6e4a415d055563f9797f
shadowmoonezios <agushifelde@hotmail.com>
2025-08-31 11:06:54 +0300

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Personenverzeichnis</h1>
+      {/* Hier können weitere Komponenten hinzugefügt werden */}
+    </div>
+  );
+};
+
+export default App;
